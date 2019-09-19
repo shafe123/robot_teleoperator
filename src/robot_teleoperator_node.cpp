c1060dec93c34e72f8f342dcf46e5b695a5e5eb9
Ethan Shafer <ethan.shafer@case.edu>
2019-09-19 15:25:05 -0400

    Fixed bug with not resetting stop movement boolean.

--- src/robot_teleoperator_node.cpp
+++ src/robot_teleoperator_node.cpp
@@ -3,6 +3,7 @@
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/LaserScan.h"
 #include <cmath>
+#include <string>
 
 bool g_teleoperator_twist_msg_received;
 bool g_teleoperator_lidar_msg_received;
@@ -35,9 +36,11 @@ void lidarCallback(const sensor_msgs::LaserScan& msg)
     double max_index;
     int value_range = (int)(abs(msg.angle_max - msg.angle_min)/msg.angle_increment);
     int iterator;
+    bool angular_check = false;
 
     ROS_DEBUG("Angle min: %f, Angle max %f, Angle incr.: %f", msg.angle_min, msg.angle_max, msg.angle_increment);
     ROS_DEBUG("Calculated %i indexes", value_range);
+
 
     for (iterator = 0; iterator <= value_range; iterator++)
     {
@@ -45,35 +48,37 @@ void lidarCallback(const sensor_msgs::LaserScan& msg)
         float x_component = msg.ranges[iterator]*cos(msg.angle_min+msg.angle_increment*iterator);
         // check if any lidar measurement will run us into wall
 	    if (msg.ranges[iterator] < msg.range_max 
-            && abs(y_component) < 0.20 
+            && abs(y_component) < 0.21 
             && x_component > 0.0
             && x_component < 0.5)
         {
-            ROS_INFO_THROTTLE(1, "Wall too close, stopping linear motion.");
-            g_stop_linear_motion = true;
+            ROS_WARN_THROTTLE(1, "Wall too close, stopping linear motion.");
+            angular_check = true;
         }
     }
+    g_stop_linear_motion = angular_check;
     g_teleoperator_lidar_msg_received = true;
 }
 
 int main(int argc, char **argv)
 {
+    ROS_INFO("%i arguments read", std::max(argc-1, 0));
     std::string TOPIC_NAME;
     int FAIL_TIME;
     std::string NAMESPACE;
     int ERROR_NOTIFICATION_TIME = 5;
 
-    if (argc != 3)
+    if (argc != 4)
     {
         ROS_INFO("Did not detect arguments, using defaults");
         TOPIC_NAME = "des_vel";
         FAIL_TIME = 60;
         NAMESPACE = "robot0";
     }
-    else if (argc == 3)
+    else if (argc == 4)
     {
         TOPIC_NAME = argv[1];
-        FAIL_TIME = std::stoi(argv[2]);
+        FAIL_TIME = atoi(argv[2]);
         NAMESPACE = argv[3];
     }
 
@@ -92,7 +97,7 @@ int main(int argc, char **argv)
     geometry_msgs::Twist no_forward_twist;
     no_forward_twist.linear = ZERO_VECTOR;
 
-    ros::init(argc, argv, "robot_teleoperator_default");
+    ros::init(argc, argv, "robot_teleoperator_default", ros::init_options::AnonymousName);
     ros::NodeHandle robot_namespace(NAMESPACE);
     ros::NodeHandle global_namespace;
     ros::Publisher pub = robot_namespace.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
@@ -111,7 +116,7 @@ int main(int argc, char **argv)
         {
             g_teleoperator_lidar_msg_received = false;
             lidar_msg_not_received_count = 0;
-            critical_fail = true;
+            critical_fail = false;
         }
         else
         {
@@ -137,8 +142,11 @@ int main(int argc, char **argv)
                          no_forward_twist.angular.y, no_forward_twist.angular.z);
                 pub.publish(no_forward_twist);
             }
-            g_teleoperator_twist_msg_received = false;
+        }
+        if(g_teleoperator_twist_msg_received)
+        {
             twist_msg_not_received_count = 0;
+            g_teleoperator_twist_msg_received = false;
         }
         else
         {
